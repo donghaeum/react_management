320c49f8faf6302b55c19d2049eb1f0d41f77062
donghaeum <edh1462@gmail.com>
2021-07-28 15:46:22 +0900

    Update files

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -9,6 +9,14 @@ import { TableRow } from '@material-ui/core';
 import { TableCell } from '@material-ui/core';
 import { CircularProgress } from '@material-ui/core';
 import { withStyles } from '@material-ui/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { alpha, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
 const styles = theme => ({
   root: {
@@ -20,7 +28,56 @@ const styles = theme => ({
   },
   progress: {
     // margin: theme.spacing(2)
-  }
+  },
+  menuButton: {
+    // marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    // [theme.breakpoints.up('sm')]: {
+    //   display: 'block',
+    // },
+  },
+  search: {
+    position: 'relative',
+    // borderRadius: theme.shape.borderRadius,
+    // backgroundColor: alpha(theme.palette.common.white, 0.15),
+    // '&:hover': {
+    //   backgroundColor: alpha(theme.palette.common.white, 0.25),
+    // },
+    marginLeft: 0,
+    width: '100%',
+    // [theme.breakpoints.up('sm')]: {
+    //   // marginLeft: theme.spacing(1),
+    //   width: 'auto',
+    // },
+  },
+  searchIcon: {
+    // padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    // padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    // paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    // transition: theme.transitions.create('width'),
+    width: '100%',
+    // [theme.breakpoints.up('sm')]: {
+    //   width: '12ch',
+    //   '&:focus': {
+    //     width: '20ch',
+    //   },
+    // },
+  },
 })
 
 
@@ -49,47 +106,77 @@ class App extends Component {
     const {completed} = this.state;
     this.setState({ completed: completed >= 100 ? 0 : completed + 1});
   }
-
+  
   render() {
     const {classes} = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>id</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>제목</TableCell>
-              <TableCell>연재 시작일</TableCell>
-              <TableCell>최신화 연재일</TableCell>
-              <TableCell>장르</TableCell>
-              <TableCell>플랫폼</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {
-              this.state.webtoons ? this.state.webtoons.map(w => {
-                return (
-                  <Webtoon 
-                    id={w.id}
-                    image={w.image}
-                    title={w.title}
-                    pdate={w.pdate}
-                    ldate={w.ldate}
-                    genre={w.genre}
-                    platform={w.platform}
-                  />
-                )
-              }) : 
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              Webtoon Search
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Paper>
+          <Table className={classes.table}>
+            <TableHead>
               <TableRow>
-                <TableCell colSpan="7" align="center">
-                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-                </TableCell>
+                <TableCell>id</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>제목</TableCell>
+                <TableCell>연재 시작일</TableCell>
+                <TableCell>최신화 연재일</TableCell>
+                <TableCell>장르</TableCell>
+                <TableCell>플랫폼</TableCell>
               </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
+            </TableHead>
+            <TableBody>
+              {
+                this.state.webtoons ? this.state.webtoons.map(w => {
+                  return (
+                    <Webtoon 
+                      id={w.id}
+                      image={w.image}
+                      title={w.title}
+                      pdate={w.pdate}
+                      ldate={w.ldate}
+                      genre={w.genre}
+                      platform={w.platform}
+                    />
+                  )
+                }) : 
+                <TableRow>
+                  <TableCell colSpan="7" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                  </TableCell>
+                </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
       
     )
   }
